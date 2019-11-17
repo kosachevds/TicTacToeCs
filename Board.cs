5e16ecb549cf15b4c383c645023b99122d7948b9
Kosachev Daniil <kos-dan@mail.ru>
2019-11-17 21:59:01 +0300

    Method CheckWin

--- Board.cs
+++ Board.cs
@@ -30,5 +30,19 @@ namespace TicTacToe
                 }
             }
         }
+
+        private bool CheckWin(Mark mark)
+        {
+            return
+                (this.Table[0] == mark && this.Table[1] == mark && this.Table[2] == mark) ||
+                (this.Table[3] == mark && this.Table[4] == mark && this.Table[5] == mark) ||
+                (this.Table[6] == mark && this.Table[7] == mark && this.Table[8] == mark) ||
+                (this.Table[0] == mark && this.Table[3] == mark && this.Table[6] == mark) ||
+                (this.Table[1] == mark && this.Table[4] == mark && this.Table[7] == mark) ||
+                (this.Table[2] == mark && this.Table[5] == mark && this.Table[8] == mark) ||
+                (this.Table[0] == mark && this.Table[4] == mark && this.Table[8] == mark) ||
+                (this.Table[2] == mark && this.Table[4] == mark && this.Table[6] == mark);
+        }
+
     }
 }
