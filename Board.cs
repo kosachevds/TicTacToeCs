7e76bc419c85e4823fd4d36b245ce4be81dd5a41
Kosachev Daniil <kos-dan@mail.ru>
2019-11-17 21:42:29 +0300

    Add property and ctor

--- Board.cs
+++ Board.cs
@@ -2,6 +2,17 @@ namespace TicTacToe
 {
     public class Board
     {
+        private const int BoardWidth = 3;
 
+        public Board()
+        {
+            this.Marks = new Mark[BoardWidth * BoardWidth];
+            for (var i = 0; i < this.Marks.Length; ++i)
+            {
+                this.Marks[i] = Mark.Empty;
+            }
+        }
+
+        public Mark[] Marks { get; }
     }
 }
