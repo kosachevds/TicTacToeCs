8daa1d6486b441e3c26a80225077495edc959290
Даниил Косачев <kdaniil@MCS>
2019-11-19 13:29:28 +0300

    Add extensions class

--- Mark.cs
+++ Mark.cs
@@ -4,4 +4,20 @@ namespace TicTacToe
     {
         Cross, Zero, Empty
     }
+
+    public static class MarkExtension
+    {
+        public static Mark GetOpponentMark(this Mark mark)
+        {
+            if (mark == Mark.Cross)
+            {
+                return Mark.Zero;
+            }
+            if (mark == Mark.Zero)
+            {
+                return Mark.Cross;
+            }
+            return Mark.Empty;
+        }
+    }
 }
