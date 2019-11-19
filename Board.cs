b4cfdc747f75e36e8869d7ca397b7c784a8cadcc
Даниил Косачев <kdaniil@MCS>
2019-11-19 13:40:38 +0300

    new ctor

--- Board.cs
+++ Board.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -6,6 +7,11 @@ namespace TicTacToe
     public class Board
     {
         private const int TableWidth = 3;
+
+        private Board(Mark[] table)
+        {
+            this.Table = (Mark[])table.Clone();
+        }
 
         public Board()
         {
