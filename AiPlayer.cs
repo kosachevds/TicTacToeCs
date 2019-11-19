6181e545d9bdb4781ddd95964c1d334e33b9316e
Даниил Косачев <kdaniil@MCS>
2019-11-19 14:12:15 +0300

    New method FindSolution

--- AiPlayer.cs
+++ AiPlayer.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace TicTacToe
 {
@@ -24,7 +26,36 @@ namespace TicTacToe
 
         private Solution FindSolution(Board board, Mark mark)
         {
+            var winner = board.GetWinner();
+            if (winner == this._mark)
+            {
+                return new Solution(AiWinScore);
+            }
+            if (winner == this._opponent)
+            {
+                return new Solution(AiLoseScore);
+            }
+            var emptyCells = board.GetEmptyIndeces();
+            if (!emptyCells.Any())
+            {
+                return new Solution(NoOneWin);
+            }
 
+            var nextBoard = board.Copy();
+            var solutions = new List<Solution>();
+            foreach (var index in emptyCells)
+            {
+                nextBoard.Table[index] = mark;
+                var possibleSolution = FindSolution(board, mark.GetOpponentMark());
+                possibleSolution.Index = index;
+                solutions.Add(possibleSolution);
+                nextBoard.Table[index] = Mark.Empty;
+            }
+
+            if (mark == this._mark) {
+                return Utils.FindWithMax(solutions, x => x.Score);
+            }
+            return Utils.FindWithMin(solutions, x => x.Score);
         }
     }
 }
