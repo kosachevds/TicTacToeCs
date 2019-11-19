c33e8884b5438ea5e2f3032b109ac14bebcc7b8b
Даниил Косачев <kdaniil@MCS>
2019-11-19 16:07:07 +0300

    Added fields and ctor

--- ConsoleClient.cs
+++ ConsoleClient.cs
@@ -1,7 +1,16 @@
+using System;
+
 namespace TicTacToe
 {
     public class ConsoleClient
     {
+        private Mark _humanMark;
+        private Mark _aiMark;
 
+        public ConsoleClient(Mark humanMark, Mark aiMark)
+        {
+            this._humanMark = humanMark;
+            this._aiMark = aiMark;
+        }
     }
 }
