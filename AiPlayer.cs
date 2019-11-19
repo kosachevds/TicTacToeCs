98f9a5d4aef33cbf97e16b9de87e02b442190734
Даниил Косачев <kdaniil@MCS>
2019-11-19 13:32:40 +0300

    Added score consts and opponent marks

--- AiPlayer.cs
+++ AiPlayer.cs
@@ -1,12 +1,20 @@
-﻿namespace TicTacToe
+﻿using System.Linq;
+
+namespace TicTacToe
 {
     public class AiPlayer
     {
+        private const int AiWinScore = 1;
+        private const int AiLoseScore = -1;
+        private const int NoOneWin = 0;
+
         private readonly Mark _mark;
+        private readonly Mark _opponent;
 
         public AiPlayer(Mark mark)
         {
             this._mark = mark;
+            this._opponent = mark.GetOpponentMark();
         }
 
         public int DoStep(Board board)
